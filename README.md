9c2d9292e3e522989ea6febec32ac50392ead878
torgil01 <torgil.vangberg@gmail.com>
2023-11-02 16:44:16 +0100

    working code

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # MID_reader
 Read csv logfiles from MID paradigm and convert to SPM design
+
+
+## Usage 
+
+```sh
+python convert_mid.py -c MID_example.csv -o mid.txt
+```
+
+This will read the input file "MID_example.csv" and convert it to a
+SPM-type onsets file on the form "id_<id_num>_<datestr>_mid.txt" e.g. 
+the id numer and date of experiement is insered in the filename 
+
+The SPM onsets file is a simple text file with three columns, 
+1. the trial number. **Note ** this has to match the order of trials in the SPM design (see below) 
+2. onset time in seconds (remember to specify timing in sec to SPM)
+3. duration of trial in seconds
+
+### Nubering of trail types
+```
+     1  reward.low
+     2  reward.high
+     3  neutral
+     4  loss.low
+     5  loss.high
+```
+
--- convert_mid.py
+++ convert_mid.py
@@ -1,70 +1,113 @@
-# the program read a mid .csv logfile and convert it to 
+# the program read a mid .csv logfile and convert it to
 # a SPM design file
 
-import sys
-import os
-import csv
+
 import argparse
 import numpy as np
 import pandas as pd
+import time
+
+from datetime import datetime
 
 
 def parse_csv(data):
-    # header of mid file
-    # run.order.file,run.system.seconds_since_epoch,run.system.time,trial.staircase.durationFrames,trial.staircase.thisTrialN,trial.system.seconds_since_epoch,trial.system.time,trial.number,time.onset,trial.response,trial.staircase_stim_duration,trial.rt,trial.stim_duration,trial.should_nudge,trial.reward,total_earnings,fix.after.feedback.adjusted,time.trial,time.run,time.global,subid,session,run,trial.type,fix.after.cue,fix.after.stim,fix.after.feedback,trial.cue_rt,trial.too_fast_rt,participant,session,fMRI? (yes or no),fMRI trigger on TTL? (yes or no),fMRI reverse screen? (yes or no),use ranged rewards?,use nudge for final run?,do only a single behavioral practice run?,start run (0-3),staircase start reward.low,staircase start reward.high,staircase start neutral,staircase start loss.low,staircase start loss.high,date,expName,frameRate,
-
+    # some of the relavant variables in csv file
     # trial.type
     # time.onset
-    # trial.response	
-    # trial.staircase_stim_duration	
-    # trial.rt	
-    # trial.stim_duration	
-    # trial.should_nudge	
-    # trial.reward	
+    # trial.response
+    # trial.staircase_stim_duration
+    # trial.rt
+    # trial.stim_duration
+    # time.trial
+    # trial.should_nudge
+    # trial.reward
     # total_earnings
+    
 
-    # create a pandas dataframe to store the data
-    df = pd.DataFrame(columns=['trial_type', 'onset', 'duration', 'response_time', 'response', 'reward', 'total_earnings'])
-    return df
+    # remove rows with NA in trial.number column
+    data = data.dropna(subset=["trial.number"], ignore_index=True)
+    print(data)
+
+    # sort by trial.number
+    data = data.sort_values(by=["trial.number"])
+    # subtract the first trial onset from all onsets
+    data["time.onset"] = data["time.onset"] - data["time.onset"][0]
+
+    # data["time.onset"] = data["time.onset"] - data["time.onset"][0]
+    # get subject id from subid column
+    id = "id_" + "%03d" % data["subid"][0]
+    # get date and time from trial.system.time
+    dd = str(data["trial.system.time"][0])
+    d =  datetime.strptime(dd, "%a %b %d %H:%M:%S %Y")
+    datestr = d.strftime("%Y%m%d")
+
+    # replace spaces in datestr with underscores
+    datestr = datestr.replace(" ", "_")
+    # create new dataframe from data that only contain the columns trial.type, time.onset, time.trial
+    df = data[["trial.type", "time.onset", "time.trial"]]
+    # sort df by time.onset
+    df = df.sort_values(by=["time.onset"])
+    # reset index
+    df = df.reset_index(drop=True)
+    return df, id, datestr
+
 
 def write_spm(df, out_file):
-        # spm design file specs: https://imaging.mrc-cbu.cam.ac.uk/imaging/BasicOnsetFiles
-        # the file is a tab-delimited text file with the following columns:
-        # trial_type onset duration 
-        #  trial_type: the name of the condition has to be a number
-        #  onset: the onset of the event in seconds
-        #  duration: the duration of the event in seconds
-    
-    df.to_csv(out_file, sep='\t', index=False)
-    
+    # spm design file specs: https://imaging.mrc-cbu.cam.ac.uk/imaging/BasicOnsetFiles
+    # the file is a tab-delimited text file with the following columns:
+    # trial_type onset duration
+    #  trial_type: the name of the condition has to be a number
+    #  onset: the onset of the event in seconds
+    #  duration: the duration of the event in seconds
 
-    # open file to write
-    f = open(out_file, 'w')
-    # write header
-    f.write('trial_type\tonset\tduration\n')
-    # write data
+    # must recode trial.type to numbers
+    # 1 = reward.low
+    # 2 = reward.high
+    # 3 = neutral
+    # 4 = loss.low
+    # 5 = loss.high
 
+    # create new column in df with trial.type recoded to numbers 
+    df["trial.type"] = df["trial.type"].replace(
+        {
+            "reward.low": 1,
+            "reward.high": 2,
+            "neutral": 3,
+            "loss.low": 4,
+            "loss.high": 5,
+        }
+    )
 
+    df.to_csv(out_file, sep="\t", index=False, float_format="%8.1f", header=False)
 
 
 def main():
     # parse input arguments
-    parser = argparse.ArgumentParser(description='Convert mid .csv logfile to SPM design file')
-    parser.add_argument('csv', help='input .csv logfile')
-    parser.add_argument('out', help='Name of SPM design file')
+    parser = argparse.ArgumentParser(
+        description="Convert mid .csv logfile to SPM design file"
+    )
+    parser.add_argument(
+        "-c", help="input .csv logfile", action="store", dest="csv", required=True
+    )
+    parser.add_argument(
+        "-o", help="Name of SPM design file", action="store", dest="out", required=True
+    )
 
     args = parser.parse_args()
     # put arguments into variables
     csv_file = args.csv
     out_file = args.out
 
-    # read csv file
-    with open(csv_file, 'r') as f:
-        reader = csv.reader(f)
-        data = list(reader)
+    # read csv file into pandas dataframe
+    data = pd.read_csv(csv_file)
 
-    # call function to parse the csv file and return an pandas df with the data
-    df = parse_csv(data)
+    # call function to parse the csv file and return a pandas df with relevant data
+    df, id, datestr = parse_csv(data)
+
+    # combine id and datestr to create a unique name for the output file
+    out_file = str(id) + "_" + datestr + "_" + out_file
+
+    print(df)
 
     # call function to write the SPM design file
     write_spm(df, out_file)
